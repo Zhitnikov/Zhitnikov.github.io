821d524198ba689274655d31cb79c8a7ca16e5e7
DarkAngel <zhitnikov0109@gmail.com>
2025-10-16 01:14:00 +0300

    resume

--- assets/index-D9QVS125.js
+++ assets/index-D9QVS125.js
@@ -1,7 +1,7 @@
 (function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))s(t);new MutationObserver(t=>{for(const e of t)if(e.type==="childList")for(const c of e.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&s(c)}).observe(document,{childList:!0,subtree:!0});function i(t){const e={};return t.integrity&&(e.integrity=t.integrity),t.referrerPolicy&&(e.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?e.credentials="include":t.crossOrigin==="anonymous"?e.credentials="omit":e.credentials="same-origin",e}function s(t){if(t.ep)return;t.ep=!0;const e=i(t);fetch(t.href,e)}})();const a="/assets/java_logo-Dmd8tdpe.png",r="/assets/my_photo-P6IYGWKh.png";function l(n){let o=0;const i=s=>{o=s,n.innerHTML=`count is ${o}`};n.addEventListener("click",()=>i(o+1)),i(0)}document.querySelector("#app").innerHTML=`
   <section class="resume">
     <section class="main-info-block">
-      <img class="main-photo" src="${a}" alt="photo">
+      <img class="main-photo" src="${r}" alt="photo">
       
       <div class="main-info-det">
         <div class="main-info-det-vac">
@@ -14,7 +14,7 @@
               Java-разработчик
             </h5>
           </div>
-          <img class="main-info-det-java-logo" src="${r}" alt="java_logo">
+          <img class="main-info-det-java-logo" src="${a}" alt="java_logo">
         </div>
         <div class="main-info-contacts">
           <h4 class="main-info-contacts-text">
